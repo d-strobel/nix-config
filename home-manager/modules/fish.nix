00f661c8474edfdd35e8dfad29cc7c305180672d
d-strobel <github.6f1aa@slmails.com>
2025-02-20 23:02:19 +0100

    feat: add fish shell alias for sudo to doas

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -2,5 +2,8 @@
   programs.fish = {
     enable = true;
     package = with pkgs; fish;
+    shellAliases = {
+      sudo = "doas";
+    };
   };
 }
--- hosts/noxus/configuration.nix
+++ hosts/noxus/configuration.nix
@@ -114,14 +114,16 @@ in {
 
   # Replace sudo with doas
   security.sudo.enable = false;
-  security.doas.enable = true;
-  security.doas.extraRules = [
-    {
-      users = ["${username}"];
-      keepEnv = true;
-      persist = false;
-    }
-  ];
+  security.doas = {
+    enable = true;
+    extraRules = [
+      {
+        groups = ["wheel"];
+        keepEnv = true;
+        persist = false;
+      }
+    ];
+  };
 
   # Additional system packages
   environment.systemPackages = with pkgs; [
@@ -155,6 +157,41 @@ in {
       variant = "";
     };
   };
+
+  security.pki.certificates = [
+    ''
+      -----BEGIN CERTIFICATE-----
+      MIIFMTCCAxmgAwIBAgIQbcBj3E7Pi5pAbwnfTfbGjjANBgkqhkiG9w0BAQwFADAx
+      MQswCQYDVQQGEwJERTEMMAoGA1UEChMDU0RLMRQwEgYDVQQDEwtTREstUk9PVC1D
+      QTAeFw0yNDAxMjIxMzU5MDdaFw00MDAxMjkwOTEzMDdaMDExCzAJBgNVBAYTAkRF
+      MQwwCgYDVQQKEwNTREsxFDASBgNVBAMTC1NESy1ST09ULUNBMIICIjANBgkqhkiG
+      9w0BAQEFAAOCAg8AMIICCgKCAgEApIl5rIgx1mqfmX+w9mjV/Ol53lgox7HJPL/u
+      VWldfKWfGirtyfEromq8loOSPc/5fhRo+AJ7Pmn3KTCBS6vAg4g+VpuYhM+hKZRh
+      bndixlb/8Gap1BgHJMRtuKxmaW5xGqNXy8yw2HI1h0Jie1LdE93n1yYwmURv9z1G
+      IWrkTTYSKc9rhScMM2bWqe6c2UOK+NxDExeyLuUauZeYT9EOuypqIfiCTYC9QTyV
+      7vi/M2VvOqwFOFFA37yy6OIFGLYxXC1TtzOR1NEw+PZ89qqXVEIisAgIwU/z1WZ9
+      yglByFmFT5+Vlh+DAtMcSHLKNppvdWcsrmh72ueVZOsNklhogZF+QCqRWfy6yDW6
+      3vKQoM1Cfh3RLh3L0ZE1igIGgTq0rNJfU4VFjgtCzKGX6K5YPRnH4Dl6Bfwguh3a
+      z0JSopRc3mD1bC8PIWJlD0A/5iX34X9EARWrB9/7/oH9qZZ8/f42YYeZucPMu2SI
+      y2IeU97ryY2sORTmnLQsUgve3o1DgDWaD0toImcLv4fvJODN8fZi8r2wyDNub7Ht
+      9uc7ewlx4W7WFPNDyNg9hZfH1rV2IX9AkOxhNxrBDFtc73lno2nBNDAchsUAgZSk
+      gmywv2615WQ3Qp+QOy09bkL9wCEx5wq+wboccrpKa7XiB6Bs68Bwdw4BWP1Arey/
+      AoAd9bUCAwEAAaNFMEMwDgYDVR0PAQH/BAQDAgEGMBIGA1UdEwEB/wQIMAYBAf8C
+      AQEwHQYDVR0OBBYEFKRSg1yGDWYa0ASqRPFcYGVGVApPMA0GCSqGSIb3DQEBDAUA
+      A4ICAQAwPzjvtB347N44wLNthKylU45rBuNJzpcgXthiEHMUmGcoIvoshoLtd+Us
+      j6jw5bMDTAKZetYXJeH0U6MOAma+Tk+7EFmwWzy6QZOcjo8s02MSj7Uc30uuw9qJ
+      tx2Y5Je0GFZVajL+pxGmfiIKpoTsqWxQwoRVZzjGmnO7dGa+BEGDZEnWPlpGC1gU
+      P8FTHu8xhRJUQwBinjzmtCSuAT4o0Qm8nfDuuNctOKtqzCJNaH+hL2C23dMo7wft
+      EMReYpHiuHkN8jGxm17dVFpvktVOiJ3quLNnkgNyrAviXuJr7ysR/xxkuV5M7J+0
+      jJIikg3CvIbHBBFrOmfUOLj331Rep3LnPB6bFhLH51FW1crlVIoeg6qK0eioof2Q
+      wW9zdHbAEAPMQiuevl2NIHpMbD/xXqPz2gd3qPHg2pbFI2zX6CFvZ7P9YDaWsTTW
+      g6LYGg6guRdLJu5fpjIdgE8ydl94mUmSC7AvniHvbB6P7aeinRL2tgXhjA/vb7ii
+      IjbIb9biwZShF++F/g0VROTr60BZE8uPrOX7Aepxf151vMoOCXmkodpKAwBpBjgr
+      5tA6ifj7674xAk1SRErSeJtIOppYy5yNT60GIzxtlgLKw1urwnf2SEFw/oPgGer6
+      RcruEJbrQVl7mKPNJ1aAFWYLOOeRdW/28ZYKgwF7D/UrepKN6w==
+      -----END CERTIFICATE-----
+    ''
+  ];
 
   # Configure console keymap
   console.keyMap = "de";
