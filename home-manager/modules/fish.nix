01abd9dc5dc8309adff4d6be5205aecb458d1cde
d-strobel <github.6f1aa@slmails.com>
2025-02-23 11:16:33 +0100

    feat(fish): add tmux script for starting from fish

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -1,9 +1,40 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.fish = {
     enable = true;
     package = with pkgs; fish;
-    # shellAliases = {
-    #   sudo = "doas";
-    # };
+    shellAliases = let
+      tmux = "${pkgs.tmux}/bin/tmux";
+      tmuxOpen =
+        pkgs.writeShellScriptBin
+        /*
+        bash
+        */
+        "tmux-open" ''
+          if [[ $PWD == $HOME ]]; then
+            selected_name="scratch"
+          else
+            selected_name=$(basename "$PWD" | tr . _)
+          fi
+
+          tmux_running=$(pgrep tmux)
+
+          if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+            ${tmux} new-session -s $selected_name -c $PWD
+            exit 0
+          fi
+
+          if ! ${tmux} has-session -t=$selected_name 2> /dev/null; then
+            ${tmux} new-session -ds $selected_name -c $PWD
+          fi
+
+          ${tmux} attach-session -t $selected_name
+        '';
+    in {
+      tmux = "${lib.getExe tmuxOpen}";
+    };
   };
 }
