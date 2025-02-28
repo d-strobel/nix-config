f9a7a67e88aa99a217e5ea63f601af1cd3934c20
d-strobel <github.6f1aa@slmails.com>
2025-02-28 15:21:27 +0100

    feat: remove hyprsunset systemd service

--- home-manager/modules/hyprsunset.nix
+++ home-manager/modules/hyprsunset.nix
@@ -2,41 +2,4 @@
   home.packages = with pkgs; [
     hyprsunset
   ];
-
-  systemd.user.services = let
-    hyprsunset = "${pkgs.hyprsunset}/bin/hyprsunset";
-  in {
-    hyprsunset = {
-      Unit = {
-        Description = "Blue-light filter on Hyprland.";
-        Documentation = "https://wiki.hyprland.org/Hypr-Ecosystem/hyprsunset";
-        PartOf = ["graphical-session.target"];
-        Requires = ["graphical-session.target"];
-        After = ["graphical-session.target"];
-        Wants = ["hyprsunset.timer"];
-      };
-
-      Service = {
-        ExecStart = "${hyprsunset} -t 5500";
-        ExecStop = "pgrep hyprsunset && pkill hyprsunset";
-        Type = "simple";
-        Restart = "on-failure";
-      };
-    };
-  };
-
-  systemd.user.timers = {
-    hyprsunset = {
-      Unit = {
-        Description = "Blue-light filter on Hyprland.";
-        Documentation = "https://wiki.hyprland.org/Hypr-Ecosystem/hyprsunset";
-      };
-
-      Timer = {
-        OnCalendar = "*-*-* 22:00:00";
-        Persistent = true;
-      };
-      Install.WantedBy = ["timers.target"];
-    };
-  };
 }
