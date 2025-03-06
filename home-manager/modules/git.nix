499dde8b2094e3ade6c10ddadadd7b3cdbd59a90
d-strobel <github.6f1aa@slmails.com>
2025-03-06 20:56:09 +0100

    feat(git): add bare repos to global config

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -4,5 +4,10 @@
     package = with pkgs; git;
     userName = "d-strobel";
     userEmail = "github.6f1aa@slmails.com";
+    extraConfig = {
+      core = {
+        bare = true;
+      };
+    };
   };
 }
