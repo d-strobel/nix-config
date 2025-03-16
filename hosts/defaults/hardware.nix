9c265b540e56036157de8f31b4137fec76eef5a4
d-strobel <github.6f1aa@slmails.com>
2025-03-16 20:30:07 +0100

    feat(nixos): switch from tlp to cpu-autofreq

--- hosts/defaults/hardware.nix
+++ hosts/defaults/hardware.nix
@@ -19,28 +19,17 @@
   };
 
   # Laptop power management
-  services.tlp = {
+  services.auto-cpufreq = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
-
-      # Hyprland fps drop issue
-      # https://wiki.hyprland.org/Configuring/Performance/#low-fpsstutterfps-drops-on-intel-igpu-with-tlp-mainly-laptops
-      # INTEL_GPU_MIN_FREQ_ON_AC = 500;
-      # INTEL_GPU_MIN_FREQ_ON_BAT = 500;
-
-      # Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 20; # 40 and below it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
     };
   };
 }
