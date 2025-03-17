e22f7e879ff2a67914566213d62b15634a68b623
d-strobel <github.6f1aa@slmails.com>
2025-03-18 00:39:08 +0100

    feat(nixos): remove tlp again due to performance issues

--- hosts/defaults/hardware.nix
+++ hosts/defaults/hardware.nix
@@ -17,21 +17,4 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  # Laptop power management
-  services = {
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_BOOST_ON_AC = 1;
-        CPU_BOOST_ON_BAT = 0;
-        CPU_HWP_DYN_BOOST_ON_AC = 1;
-        CPU_HWP_DYN_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-      };
-    };
-  };
 }
