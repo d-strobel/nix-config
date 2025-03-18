500ee6de19fdd8adb3c85ecacd48623977fd8a87
d-strobel <github.6f1aa@slmails.com>
2025-03-18 10:13:36 +0100

    feat(fish): rename tmux-open to t

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -34,7 +34,7 @@
           ${tmux} attach-session -t $selected_name
         '';
     in {
-      tmux-open = "${lib.getExe tmuxOpen}";
+      t = "${lib.getExe tmuxOpen}";
       gbws = "git-branch-worktree-switch";
     };
 
