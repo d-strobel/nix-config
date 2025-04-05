29b6664c1264e5d4de1cc439c5a2d771c8f5b355
d-strobel <github.6f1aa@slmails.com>
2025-04-06 00:52:12 +0200

    feat(neovim): remove deprecated options

--- home-manager/dotfiles/nvim/lua/plugins/lsp.lua
+++ home-manager/dotfiles/nvim/lua/plugins/lsp.lua
@@ -22,17 +22,6 @@ return {
       local username = "dstrobel"
       local hostname = "noxus"
 
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover, {
-          border = "single"
-        }
-      )
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-        vim.lsp.handlers.signature_help, {
-          border = "single"
-        }
-      )
-
       -- Lua
       lspconfig["lua_ls"].setup {
         capabilities = capabilities
