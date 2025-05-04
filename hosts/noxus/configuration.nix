ffa31436c0d8e7662829c812e0a36bc971fe58d4
d-strobel <github.6f1aa@slmails.com>
2025-05-04 18:51:36 +0200

    feat(nixos): enable firewall

--- hosts/noxus/configuration.nix
+++ hosts/noxus/configuration.nix
@@ -18,7 +18,7 @@
   };
 
   # Firewall
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
