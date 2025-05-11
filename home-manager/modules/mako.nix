73614e468410a353196784641c85a7db459dec55
d-strobel <github.6f1aa@slmails.com>
2025-05-11 20:26:57 +0200

    fix(mako): home manager module settings

--- home-manager/modules/mako.nix
+++ home-manager/modules/mako.nix
@@ -2,16 +2,18 @@
   services.mako = {
     enable = true;
     package = with pkgs; mako;
-    defaultTimeout = 5000;
-    anchor = "top-right";
-    backgroundColor = "#0A0A0FCC";
-    borderRadius = 6;
-    borderSize = 0;
-    font = "JetBrainsMono NF";
-    textColor = "#e0def4";
-    height = 150;
-    width = 350;
-    padding = "5";
-    margin = "10";
+    settings = {
+      default-timeout = 5000;
+      anchor = "top-right";
+      background-color = "#0A0A0FCC";
+      border-radius = 6;
+      border-size = 0;
+      font = "JetBrainsMono NF";
+      text-color = "#e0def4";
+      height = 150;
+      width = 350;
+      padding = "5";
+      margin = "14";
+    };
   };
 }
