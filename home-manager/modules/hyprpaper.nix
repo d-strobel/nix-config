4cc31ddfe91da84620f0fa8075c670f706411ab9
d-strobel <github.6f1aa@slmails.com>
2025-05-11 20:41:28 +0200

    chore(hyprpaper): remove preloaded wallpapers

--- home-manager/modules/hyprpaper.nix
+++ home-manager/modules/hyprpaper.nix
@@ -13,10 +13,6 @@
       splash_offset = 2.0;
 
       preload = [
-        "${config.home.homeDirectory}/.config/wallpapers/nixos-01.jpg"
-        "${config.home.homeDirectory}/.config/wallpapers/nixos-02.jpg"
-        "${config.home.homeDirectory}/.config/wallpapers/waifu-01.jpg"
-        "${config.home.homeDirectory}/.config/wallpapers/bg-01.jpg"
         "${config.home.homeDirectory}/.config/wallpapers/bg-02.jpg"
       ];
 
