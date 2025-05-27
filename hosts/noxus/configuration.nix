d855fb21204e20f526138b77a7026b418b3b45d4
d-strobel <github.6f1aa@slmails.com>
2025-05-27 18:30:24 +0200

    feat(nixos): add laptop closing the lid rules

--- hosts/noxus/configuration.nix
+++ hosts/noxus/configuration.nix
@@ -37,6 +37,13 @@
     LIBVA_DRIVER_NAME = "i965";
   };
 
+  # Laptop closing the lid:
+  # one of "ignore", "poweroff", "reboot", "halt", "kexec", "suspend", "hibernate", "hybrid-sleep", "suspend-then-hibernate", "lock"
+  services.logind.lidSwitch = "suspend";
+  services.logind.lidSwitchExternalPower = "suspend";
+  # Do nothing when other monitors are plugged in.
+  services.logind.lidSwitchDocked = "ignore";
+
   # Firewall
   networking.firewall.enable = true;
 
