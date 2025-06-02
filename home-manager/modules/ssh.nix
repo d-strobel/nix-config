01950a1045ef595426aa42d2accfd51846cbd2e9
d-strobel <github.6f1aa@slmails.com>
2025-06-02 13:58:29 +0200

    feat(ssh): add ssh config and more key-pairs

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -5,6 +5,21 @@
 }: {
   # SSH keys and config is fully driven by sops-nix
   sops.secrets = {
+    # SSH config
+    "ssh/config" = {
+      path = "${config.home.homeDirectory}/.ssh/config";
+      mode = "0600";
+    };
+
+    # SSH key-pairs
+    "ssh/keys/id_ed25519/private" = {
+      path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      mode = "0600";
+    };
+    "ssh/keys/id_ed25519/public" = {
+      path = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      mode = "0644";
+    };
     "ssh/keys/id_ed25519_vault-prod/private" = {
       path = "${config.home.homeDirectory}/.ssh/id_ed25519_vault-prod";
       mode = "0600";
