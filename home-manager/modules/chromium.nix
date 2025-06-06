9c02a773f14e01961748999c600a6843c142f695
d-strobel <github.6f1aa@slmails.com>
2025-06-06 23:07:31 +0200

    feat(browser): remove ublock origin extension from chromium

--- home-manager/modules/chromium.nix
+++ home-manager/modules/chromium.nix
@@ -4,9 +4,6 @@
     package = with pkgs; chromium;
 
     extensions = [
-      # ublock Origin
-      {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";}
-
       # Dark Reader
       {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";}
     ];
