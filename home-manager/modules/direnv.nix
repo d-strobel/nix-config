20087685ea6eb7530ad56a1f1d0e79a4f3459227
d-strobel <github.6f1aa@slmails.com>
2025-06-09 23:33:09 +0200

    feat(direnv): add secrets

--- home-manager/modules/direnv.nix
+++ home-manager/modules/direnv.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.direnv = {
     enable = true;
     package = with pkgs; direnv;
@@ -6,6 +10,20 @@
     nix-direnv = {
       enable = true;
       package = with pkgs; nix-direnv;
+    };
+  };
+
+  # Deploy envrc
+  sops.secrets = let
+    home = config.home.homeDirectory;
+  in {
+    "direnv/git/gitlab.com/strobel-iac/envrc" = {
+      path = "${home}/git/gitlab.com/strobel-iac/.envrc";
+      mode = "0600";
+    };
+    "direnv/git/github.com/envrc" = {
+      path = "${home}/git/github.com/.envrc";
+      mode = "0600";
     };
   };
 }
