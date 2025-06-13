ac7b2c31f06b5f6541485bfd6549fe745908a40c
d-strobel <github.6f1aa@slmails.com>
2025-06-14 01:52:33 +0200

    feat(nixos): add nix settings

--- hosts/defaults/nix.nix
+++ hosts/defaults/nix.nix
@@ -16,7 +16,7 @@
   in {
     settings = {
       # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
+      experimental-features = ["nix-command" "flakes" "recursive-nix" "ca-derivations"];
       # Opinionated: disable global registry
       flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
@@ -27,6 +27,19 @@
       keep-going = true;
       warn-dirty = false;
       http-connections = 0;
+      accept-flake-config = true;
+      keep-derivations = true;
+      keep-outputs = true;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
 
     # Opinionated: make flake registry and nix path match flake inputs
