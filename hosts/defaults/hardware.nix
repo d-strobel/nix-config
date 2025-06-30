a19ec0cdbd667d86437fe0db990119257dd8ec54
d-strobel <github.6f1aa@slmails.com>
2025-06-30 10:11:26 +0200

    feat(hardware): switch specific hardware stuff to optional laptop file

--- hosts/defaults/hardware.nix
+++ hosts/defaults/hardware.nix
@@ -23,27 +23,6 @@
     };
   };
 
-  # Video acceleration
-  nixpkgs.config.packageOverrides = pkgs: {
-    intel-vaapi-driver = pkgs.intel-vaapi-driver.override {
-      enableHybridCodec = true;
-    };
-  };
-  hardware.graphics = {
-    # hardware.graphics since NixOS 24.11
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      libvdpau-va-gl
-      intel-compute-runtime
-      vpl-gpu-rt
-    ];
-  };
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "i965";
-  };
-
   # Enable thunderbolt ports
   services.hardware.bolt.enable = true;
 }
--- hosts/options/laptop.nix
+++ hosts/options/laptop.nix
@@ -1,8 +1,78 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   # Laptop closing the lid:
   # one of "ignore", "poweroff", "reboot", "halt", "kexec", "suspend", "hibernate", "hybrid-sleep", "suspend-then-hibernate", "lock"
   services.logind.lidSwitch = "suspend";
   services.logind.lidSwitchExternalPower = "suspend";
   # Do nothing when other monitors are plugged in.
   services.logind.lidSwitchDocked = "ignore";
+
+  # Video acceleration
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override {
+      enableHybridCodec = true;
+    };
+  };
+  hardware.graphics = {
+    # hardware.graphics since NixOS 24.11
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+      intel-compute-runtime
+      vpl-gpu-rt
+    ];
+  };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
+  services.xserver.videoDrivers = ["modesetting" "nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Kernel package
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # For hybrid cpu/gpu
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      # Set the BusIds here.
+      # Lookup with the following command: nix-shell -p lshw --run 'sudo lshw -c display'
+      # Convert the hex codes to decimal
+      nvidiaBusId = "PCI:45:0:0";
+      intelBusId = "PCI:0:2:0";
+    };
+  };
 }
