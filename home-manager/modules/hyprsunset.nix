ca17a9b80aacd98b81344147098c08609ba7aedd
d-strobel <github.6f1aa@slmails.com>
2025-08-17 02:50:07 +0200

    feat(hyprsunset): update to home manager module with scheduling settings

--- home-manager/modules/hyprsunset.nix
+++ home-manager/modules/hyprsunset.nix
@@ -1,5 +1,21 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    hyprsunset
-  ];
+  services.hyprsunset = {
+    enable = true;
+    package = with pkgs; hyprsunset;
+    settings = {
+      max-gamma = 150;
+
+      profile = [
+        {
+          time = "7:30";
+          identity = true;
+        }
+        {
+          time = "21:00";
+          temperature = 5500;
+          gamma = 0.9;
+        }
+      ];
+    };
+  };
 }
