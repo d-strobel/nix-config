91b192e5d1426ca98f72900fed34eeec8f6bc629
d-strobel <github.6f1aa@slmails.com>
2025-08-25 16:08:18 +0200

    fix(cups): printing does not work

--- hosts/defaults/hardware.nix
+++ hosts/defaults/hardware.nix
@@ -17,6 +17,11 @@
 
   # Enable printing
   services = {
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
     printing = {
       enable = true;
       startWhenNeeded = true;
