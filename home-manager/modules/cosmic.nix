84146dad6bc05736a6ce4046dcbe1ff6df5a3e64
d-strobel <github.6f1aa@slmails.com>
2025-09-26 00:09:49 +0200

    feat(cosmic): change cosmic settings to symlinks

--- home-manager/modules/cosmic.nix
+++ home-manager/modules/cosmic.nix
@@ -1,20 +1,40 @@
-{pkgs, ...}: {
+{
+  cfg,
+  config,
+  pkgs,
+  ...
+}: let
+  # Custom symlinking
+  mkSymlinkAttrs = import ../../lib/mkSymlinkAttrs.nix {
+    inherit pkgs;
+    inherit (cfg) context runtimeRoot;
+    hm = config.lib;
+  };
+in {
   home.packages = with pkgs; [
     cosmic-edit
     cosmic-files
   ];
 
-  # Rose-Pine dark theme
-  home.file."./.config/cosmic/com.system76.CosmicTheme.Dark" = {
-    enable = true;
-    source = ../dotfiles/cosmic/com.system76.CosmicTheme.Dark;
-    recursive = true;
+  home.file = mkSymlinkAttrs {
+    ".config/cosmic" = {
+      source = ../dotfiles/cosmic;
+      outOfStoreSymlink = true;
+      recursive = true;
+    };
   };
 
-  # Cosmic editor config
-  home.file."./.config/cosmic/com.system76.CosmicEdit" = {
-    enable = true;
-    source = ../dotfiles/cosmic/com.system76.CosmicEdit;
-    recursive = true;
-  };
+  # Rose-Pine dark theme
+  # home.file."./.config/cosmic/com.system76.CosmicTheme.Dark" = {
+  #   enable = true;
+  #   source = ../dotfiles/cosmic/com.system76.CosmicTheme.Dark;
+  #   recursive = true;
+  # };
+  #
+  # # Cosmic editor config
+  # home.file."./.config/cosmic/com.system76.CosmicEdit" = {
+  #   enable = true;
+  #   source = ../dotfiles/cosmic/com.system76.CosmicEdit;
+  #   recursive = true;
+  # };
 }
