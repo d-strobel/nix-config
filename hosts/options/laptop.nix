e58df91a3ae66aaa43482ff5c4123592fc07a859
d-strobel <github.6f1aa@slmails.com>
2025-10-12 12:44:17 +0200

    chore: update laptop lid closing actions

--- hosts/options/laptop.nix
+++ hosts/options/laptop.nix
@@ -5,10 +5,12 @@
 }: {
   # Laptop closing the lid:
   # one of "ignore", "poweroff", "reboot", "halt", "kexec", "suspend", "hibernate", "hybrid-sleep", "suspend-then-hibernate", "lock"
-  services.logind.lidSwitch = "suspend";
-  services.logind.lidSwitchExternalPower = "suspend";
-  # Do nothing when other monitors are plugged in.
-  services.logind.lidSwitchDocked = "ignore";
+  services.logind.settings.Login = {
+    HandleLidSwitch = "suspend";
+    HandleLidSwitchExternalPower = "suspend";
+    # Do nothing when other monitors are plugged in.
+    HandleLidSwitchDocked = "ignore";
+  };
 
   # Video acceleration
   nixpkgs.config.packageOverrides = pkgs: {
