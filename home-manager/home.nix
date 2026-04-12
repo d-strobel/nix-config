2565a4c5ab8644627ddb0f2f3e2e7fd46358eec8
d-strobel <github.6f1aa@slmails.com>
2026-04-12 21:33:53 +0200

    chore(deps): update devpod to v0.20.1

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,11 +19,11 @@
   # Devpod (fork)
   devpod = pkgs.stdenv.mkDerivation rec {
     pname = "devpod";
-    version = "0.18.4";
+    version = "0.20.1";
 
     src = pkgs.fetchurl {
       url = "https://github.com/skevetter/devpod/releases/download/v${version}/devpod-linux-amd64";
-      sha256 = "sha256-RW1al6s/8Y3w/0hfWU3ge9EeHCq0h0eFadz7abvoRyE=";
+      sha256 = "sha256-ic1T+C6mXG8FUbzzm0LBYS1zfNcfZC3MNIT+RlXecMw=";
     };
 
     dontUnpack = true;
