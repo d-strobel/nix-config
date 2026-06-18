b5f615c9fcb781a695a1b47a137d17f8db28325d
d-strobel <github.6pr5i@slmails.com>
2026-06-18 23:50:09 +0200

    feat(darkman): add config to change GTK dark light theme

---
+++ home-manager/dotfiles/dot_local/share/darkman/01-gtk.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Switch the GTK Theme.
+
+case "$1" in
+dark)
+  THEME=Adwaita-dark
+  COLOR=prefer-dark
+  ;;
+light)
+  THEME=Adwaita
+  COLOR=prefer-light
+  ;;
+default) exit 1 ;;
+esac
+
+gsettings set org.gnome.desktop.interface color-scheme "$COLOR"
+gsettings set org.gnome.desktop.interface gtk-theme "$THEME"
