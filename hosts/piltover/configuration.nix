6359c17a6ca4596d6b9197d0ce24476d9078c054
d-strobel <github.6pr5i@slmails.com>
2026-09-26 17:06:33 +0200

    feat(usbguard): strip down usbguard config

--- hosts/piltover/configuration.nix
+++ hosts/piltover/configuration.nix
@@ -17,23 +17,22 @@
       enable = true;
       # To get blocked devices: usbguard list-devices -b
       rules = ''
-        allow id 1d6b:0002 serial "0000:00:14.0" name "xHCI Host Controller" hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" parent-hash "rV9bfLq7c2eA4tYjVjwO4bxhm+y6GgZpl9J60L0fBkY=" with-interface 09:00:00 with-connect-type ""
-        allow id 1d6b:0003 serial "0000:00:14.0" name "xHCI Host Controller" hash "XCHNkqbiDxI/M86q3Xx/1NPsFNctpAcKPF2KF9AkPpY=" parent-hash "rV9bfLq7c2eA4tYjVjwO4bxhm+y6GgZpl9J60L0fBkY=" with-interface 09:00:00 with-connect-type ""
-        allow id 1d6b:0002 serial "0000:3c:00.0" name "xHCI Host Controller" hash "+k9gUUE6Cnbob2WB/I//KMZ1hZ1UgvI6RrqNkIDvdmQ=" parent-hash "zCxLdr73Tn0YoKg15XR1ttIXizl8vMD+KtVAQnBZO8I=" with-interface 09:00:00 with-connect-type ""
-        allow id 1d6b:0003 serial "0000:3c:00.0" name "xHCI Host Controller" hash "KkQZN5DqcZCTZozZRGI/Qr09HJrHE8L5Ml5YlUl2G3Y=" parent-hash "zCxLdr73Tn0YoKg15XR1ttIXizl8vMD+KtVAQnBZO8I=" with-interface 09:00:00 with-connect-type ""
-        allow id 2109:2817 serial "" name "USB2.0 Hub             " hash "I0EyQg3EPh4pnLnd0hJMXwPi69STx6hDl43+nji0FxA=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-1" with-interface { 09:00:01 09:00:02 } with-connect-type "hotplug"
-        allow id 058f:9540 serial "" name "EMV Smartcard Reader" hash "j6z/wqFtA1bZWwBIPmIr/g8KfsEQJ63vpgf4cBcNLbU=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-3" with-interface 0b:00:00 with-connect-type "not used"
-        allow id 8087:0a2b serial "" name "" hash "TtRMrWxJil9GOY/JzidUEOz0yUiwwzbLm8D7DJvGxdg=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-7" with-interface { e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 } with-connect-type "not used"
-        allow id 04f2:b604 serial "0001" name "Integrated Camera" hash "Sra5Do2lULxlGqcVOc0E68CJLWT1st8KiYXu4dbUQoQ=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface { 0e:01:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 } with-connect-type "not used"
-        allow id 06cb:009a serial "176bb407c4ef" name "" hash "vA8TYlGOgvX8lO6gCOGyqpKROqeqveEaV5lInGaxn7Q=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface ff:00:00 with-connect-type "not used"
-        allow id 2109:0817 serial "" name "USB3.0 Hub             " hash "vQNT567XT+bPBqIu0p/etX+pnHWglTlGAjGJuRFxsxA=" parent-hash "XCHNkqbiDxI/M86q3Xx/1NPsFNctpAcKPF2KF9AkPpY=" via-port "2-1" with-interface 09:00:00 with-connect-type "hotplug"
-        allow id 0bda:0316 serial "20120501030900000" name "USB3.0-CRW" hash "WG1MSC3YZsmCslTNGpjTTjT2lUvhNfU4gEVvD3gIuV4=" parent-hash "XCHNkqbiDxI/M86q3Xx/1NPsFNctpAcKPF2KF9AkPpY=" with-interface 08:06:50 with-connect-type "not used"
-        allow id 05ac:0250 serial "" name "Keychron K2" hash "Ve+iuk3i2DIWlQQWnbTUbGbwFPk913tgx8Qpip54foA=" parent-hash "I0EyQg3EPh4pnLnd0hJMXwPi69STx6hDl43+nji0FxA=" via-port "1-1.2" with-interface { 03:01:01 03:01:02 } with-connect-type "unknown"
-        allow id 1050:0407 serial "" name "YubiKey OTP+FIDO+CCID" hash "Q+A8QQReKclmBSaDIYja0w4Bx6ld2IU6wF7HFKdtJ3Q=" parent-hash "I0EyQg3EPh4pnLnd0hJMXwPi69STx6hDl43+nji0FxA=" via-port "1-1.3" with-interface { 03:01:01 03:00:00 0b:00:00 } with-connect-type "unknown"
-        allow id 04a5:8001 serial "" name "BenQ ZOWIE Gaming Mouse" hash "Rfmn4vEZ5Jh/iqGZ739u7SpoJP3tx8iAponKBSJoKhU=" parent-hash "I0EyQg3EPh4pnLnd0hJMXwPi69STx6hDl43+nji0FxA=" via-port "1-1.4" with-interface 03:01:02 with-connect-type "unknown"
+        allow id 05ac:0250 name "Keychron K2" hash "Ve+iuk3i2DIWlQQWnbTUbGbwFPk913tgx8Qpip54foA="
+        allow id 1050:0407 name "YubiKey OTP+FIDO+CCID" hash "Q+A8QQReKclmBSaDIYja0w4Bx6ld2IU6wF7HFKdtJ3Q="
+        allow id 04a5:8001 name "BenQ ZOWIE Gaming Mouse" hash "Rfmn4vEZ5Jh/iqGZ739u7SpoJP3tx8iAponKBSJoKhU="
+        allow id 04f2:b604 serial "0001" name "Integrated Camera" hash "Sra5Do2lULxlGqcVOc0E68CJLWT1st8KiYXu4dbUQoQ="
+        allow id 06cb:009a serial "176bb407c4ef" hash "vA8TYlGOgvX8lO6gCOGyqpKROqeqveEaV5lInGaxn7Q="
+        allow id 058f:9540 name "EMV Smartcard Reader" hash "j6z/wqFtA1bZWwBIPmIr/g8KfsEQJ63vpgf4cBcNLbU="
+        allow id 058f:6387 serial "CDEE572F" name "Mass Storage" hash "fT4h6Vron885t4b7D4AQsaF8Y+w1dogoUkc9LFr6K0k="
+        allow id 0bda:0316 serial "20120501030900000" name "USB3.0-CRW" hash "WG1MSC3YZsmCslTNGpjTTjT2lUvhNfU4gEVvD3gIuV4="
+        allow id 8087:0a2b hash "TtRMrWxJil9GOY/JzidUEOz0yUiwwzbLm8D7DJvGxdg="
+        allow id 0781:5595 serial "4C530000090503101133" name "Ultra USB 3.0" hash "hbUuAy9v368Q9NjlUKt5gKTZLJMJz7Loe8ZhGN7s4V4="
+        allow id 05dc:a838 serial "AAQZC6C87ZM46YD7" name "USB Flash Drive" hash "yfdwVxGmYIUDtPbRaK9APa2QWGfxh50xyUF4AzIKzXs="
+        allow id 2109:2817 name "USB2.0 Hub" hash "I0EyQg3EPh4pnLnd0hJMXwPi69STx6hDl43+nji0FxA="
+        allow id 2109:0817 name "USB3.0 Hub" hash "vQNT567XT+bPBqIu0p/etX+pnHWglTlGAjGJuRFxsxA="
+        # allow id abcd:1234 hash "WwLIxRVjks6yVS9HHhP1Vn3rryx6WklV8RXeIlNoXag="
       '';
-      IPCAllowedGroups = [];
-      IPCAllowedUsers = ["root"];
+      IPCAllowedGroups = ["wheel"];
     };
   };
 
